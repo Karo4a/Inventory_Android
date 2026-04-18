dc5f306dbc5aea9e9d8474625ec7b4e579ac8b7c
Karo4a <t1mati666@yandex.ru>
2026-04-18 13:53:17 +0300

    Создан класс InventoryDatabase Исправил версию зависимостей `room`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,9 +82,9 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.9.7")
 
     //Room
-    implementation("androidx.room:room-runtime:2.8.4")
+    implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
     ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
-    implementation("androidx.room:room-ktx:2.8.4")
+    implementation("androidx.room:room-ktx:${rootProject.extra["room_version"]}")
 
     // Testing
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
